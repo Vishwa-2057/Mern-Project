c404233b489c2b5609f0f2c42ef967ad84563c78
Vishwa U <vishwa27032004@gmail.com>
2025-08-28 21:43:54 +0530

    Second Commit

--- index.js
+++ index.js
@@ -2,24 +2,39 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
+
+require("dotenv").config();
 
 const GameRoutes = require("./Routes/GameRoutes");
 const UserRoutes = require("./Routes/UserRoutes");
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/games",GameRoutes);
-app.use("/users",UserRoutes);
+// API Routes
+app.use("/games", GameRoutes);
+app.use("/users", UserRoutes);
 
-app.listen(4000, () => console.log("Server running on port 4000"));
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../front-end/build")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../front-end/build/index.html"));
+  });
+}
 
-
+// Start server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
